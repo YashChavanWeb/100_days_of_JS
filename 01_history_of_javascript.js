5e24c275e76e9b8d3046c469766d4280d78a687f
YashChavanWeb <yashchavan4628@gmail.com>
2024-04-20 12:20:08 +0530

    day 01

---
+++ 01_history_of_javascript.js
@@ -0,0 +1,20 @@
+// History of JavaScript 
+
+// Netscape Browser - static websites - no calculation
+// Creator of Js - Brendan Fich  - in 10 days - Mocha - LiveScript
+// Quote - Every application that can be written in Javascript will be written in Javascript
+// ECMA - made js as standardised language - should be used by all browsers
+// Chrome - own js engine (V8 engine) - more fast
+// Rayn Dahl - Took V8 engine from chrome and put it into Nodejs
+// Frameworks - React, Angular - For complex websites
+
+
+
+// Interpreted or Compiled ?
+
+// Javascript - it is interpreted language
+// Interpreted Language - Line by line codes and converts - translater - slow
+// Compiled Language - Software - gives binary file - that file can be executed at once
+// V8 Engine - Interpreted + Compiled both - jet compiler
+
+
