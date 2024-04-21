3bbf1c7ee82bfe7cfd5de7f742d26c26d0e1366e
YashChavanWeb <yashchavan4628@gmail.com>
2024-04-21 12:21:09 +0530

    day 02

---
+++ 02_intro_to_javascript.js
@@ -0,0 +1,25 @@
+// Basics:
+// Programming - set of instructions that are compiled and executed for performing a task
+// Console - REPL - read , evaluate , print , loop
+// to check what request is sent on reload - go to networks tab
+// sources tab - shows all the files that are loaded for our website
+// for calculation, javascript follows Associativity - Mul/div , add/sub (FCFS)
+
+// Printing in javascript
+console.log("Hello World")
+console.log(7+6)
+
+
+
+// Connecting HTML and JS:
+// 1. We can write js in script tag after body tag of HTML
+// 2. We can also connect js file to html by writing below syntax in head tag
+// <script src="script.js"> </script> 
+
+
+
+// Important:
+// if we write script tag above the body, then it first renders entire js and then moves to HTML
+// to avoid this we use defer keyword which completes HTML and then loads JS - DOM - interact with HTML
+// <script src="script.js" defer> </script> 
+
