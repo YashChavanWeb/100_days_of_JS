6b793d69556d75eb4ec59361e70d107131c13a6a
Yash Chavan <yashchavan4628@gmail.com>
2024-05-18 11:12:38 +0530

    day 025

---
+++ 05_Basics_and_Loops/025_while_loop.js
@@ -0,0 +1,32 @@
+// While loop
+let i = 1
+
+while (i <= 10) {
+    console.log(i)
+    i++
+}
+// Note: convention for writing a variable for loop is generally from i  (j,k, etc)
+
+
+
+
+
+
+
+// Practice Question
+
+const friends = ['Yash', 'Kamal', 'Sarth', 'Vivek']
+
+let j = 0
+while (j < friends.length) {
+    console.log(`${j + 1}. ${friends[j]}`)
+    j++;
+}
+
+
+
+
+
+// Practice Resources
+// 1. Free code camp - certification
+// 2. Front End practice.com for cloning the design of the websites
