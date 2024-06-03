3cd110c22c6556292a779f1f31d2c0f144c7f966
Yash Chavan <yashchavan4628@gmail.com>
2024-06-03 18:42:43 +0530

    day 057

---
+++ 08_DOM/057_element_vs_node/script.js
@@ -0,0 +1,48 @@
+
+// document.body.childNodes
+// - Node is an object which has various properties
+
+// document.body.childNodes[2]
+// - it comes in string but its type is an object with various properties
+
+
+// Note:
+// - an element node properties + its own extra properties
+
+// - there are various node types - element Node, text Node , attribute Node , etc
+// - and it has its own value which is 1,2,3 , etc
+
+
+
+
+
+
+
+
+
+
+
+
+// Types of Nodes
+// 1. Element Node - p tag
+// 2. Attribute Node  - style, img
+// 3. Text Node - text written without tags
+// 4. Comment Node - comments in HTML
+// 5. Document Node - DOM
+
+
+
+
+
+
+
+
+
+
+
+
+
+// It is not possible to change the properties of a text which has no tags
+// - to do so we can use node
+
+// document.body.childNodes[2].nodeValue = "This is how we change"
